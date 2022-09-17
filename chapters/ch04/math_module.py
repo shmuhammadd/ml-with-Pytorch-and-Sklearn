c83742c15c74873052cd02e79aa00d08f825d588
shamsuddeen <shmuhammad.csc@buk.edu.ng>
2022-09-17 10:21:53 +0100

    chapter 6

---
+++ chapters/ch04/math_module.py
@@ -0,0 +1,17 @@
+##
+
+def add_numbers(x, y):
+    return x + y
+
+def subtract_numbers(x, y):
+    return x - y
+
+def multiply_numbers(x, y):
+    return x * y
+
+def divide_numbers(x, y):
+    return x / y
+
+
+
+
